9b92b31e8bd41e2ed3f301e90a67b58e7b72c9fb
Lillian Blair <lilliand_2000@yahoo.com>
2020-05-29 22:51:17 -0500

    Added Navigo to Capstone Project

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ import { Header, Nav, Main, Footer } from "./components";
 
 import * as state from "./store";
 
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+
+const router = new Navigo(window.location.origin);
+
+router
+  .on({
+    ":page": (params) => render(state[capitalize(params.page)]),
+    "/": () => render(state.Home)
+  })
+  .resolve();
+
 const render = (st = state.Home) => {
   document.querySelector("#root").innerHTML = `
     ${Header(st)}
@@ -9,37 +21,27 @@ const render = (st = state.Home) => {
     ${Main(st)}
     ${Footer()}
     `;
-  console.log("render");
+
+  router.updatePageLinks();
+
+  
   addNavEventListeners();
   addNavToggle();
 };
 
 render(state.Home);
 
-//nav links event listener
-function addNavEventListeners() {
-  document.querySelectorAll("nav a").forEach((navLink) => {
-    navLink.addEventListener("click", event => {
-      event.preventDefault();
-      render(state[event.target.textContent]);
-      // it could have been this
-      // let selectedPage = event.target.textContent;
-      // let pieceOfState = state[selectedPage]
-      // render(pieceOfState);
-    });
+
+function addNavToggle() {
+  // add menu toggle to bars icon ar in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
 }
 
-function addNavToggle() {
-// add menu toggle to bars icon ar in nav bar
-    document.querySelector(".fa-bars").addEventListener("click", () => {
-    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
-}
-
 function listenForFormEvent() {
-  document.querySelectorAll("form").forEach(form =>
-    form.addEventListener("submit", event => {
+  document.querySelectorAll("form").forEach((form) =>
+    form.addEventListener("submit", (event) => {
       event.preventDefault();
       console.log(event.target);
     })

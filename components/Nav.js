c0f55d9c7409d40ddc516bd64d40814bba6d475e
Lillian Blair <lilliand_2000@yahoo.com>
2020-06-01 14:35:41 -0500

    Got rid of Navigo

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,8 @@
-export default (links) => `
+export default links => `
 <nav>
   <i class="fas fa-bars"></i>
     <ul class="hidden--mobile nav-links">
-    ${links.map(
-      link =>`<li><a href="#">${link}</a></li>`
-    ).join()}
+    ${links.map(link => `<li><a href="#">${link}</a></li>`).join()}
 
     </ul>
   </nav>

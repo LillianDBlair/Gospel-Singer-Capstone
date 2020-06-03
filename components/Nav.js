ae66d69160b562d6e5afd13af64b1c88f778fde3
Lillian Blair <lilliand_2000@yahoo.com>
2020-06-03 16:58:00 -0500

    Made changes to Components/Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,9 @@
-export default (links) => `
+export default links => `
 <nav>
   <i class="fas fa-bars"></i>
     <ul class="hidden--mobile nav-links">
     ${links
-      .map((link) => `<li><a href="/${link}" data-navigo>${link}</a></li>`)
+      .map(link => `<li><a href="/${link}" data-navigo>${link}</a></li>`)
       .join("")}
     </ul>
   </nav>

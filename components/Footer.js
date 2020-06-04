db725ed710557802c26339f5af35c5e6c8871129
Lillian Blair <lilliand_2000@yahoo.com>
2020-06-04 12:37:00 -0500

    Made changes to footer links

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 export default () => `
 <footer id="footer"><p style="font-size: 50%;">&copy;Capstone Project 2020</p>
-  <a href="https://twitter.com/explore" class="fa fa-twitter"></a>
-  <a href="https://www.linkedin.com/home" class="fa fa-linkedin"></a>
-  <a href="https://https://www.instagram.com/" class="fa fa-instagram"></a>
+  <a href="https://twitter.com/lilliand_2000" class="fa fa-twitter"></a>
+  <a href="www.linkedin.com/in/lilliandblair" class="fa fa-linkedin"></a>
+  <a href="https://www.instagram.com/lillianblair2000/?hl=en" class="fa fa-instagram"></a>
   <a href="https://github.com/LillianDBlair/Gospel-Singer-Capstone" class="fa fa-github"></a>
 </footer>
 `;

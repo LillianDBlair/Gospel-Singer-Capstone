4a26920c1ab2c4c59d8dd82076c6dbffb47fc017
Lillian Blair <lilliand_2000@yahoo.com>
2020-06-19 12:44:30 -0500

    Made changes to Home.js

--- components/views/Home.js
+++ components/views/Home.js
@@ -5,37 +5,37 @@ export default () => `
           <p class="center" style="fontsize:30px"><b>Song's I Love to Sing</b></p>
 
               <div class="flex">
-                  <p>You Can Make It</p>
-                  <p>There Is None like You</p>
-                  <p>Be Cause He lives</p>
-                  <p>Because of Who You Are</p>
-                  <p>We Exalt Thee</p>
-                  <p>Trust Me</p>
-                  <p>I Won't Complain</p>
-                  <p>I Command My Soul Bless the Lord</p>
-                  <p>Sometimes You Have to Encourage Yourself</p>
-                  <p>Our God Is An Awesome God</p>
-                  <p>Let Jesus Fill This Place</p>
-                  <p>Overflows</p>
-                  <p>Pure Love</p>
-                  <p>Glory To Your Name</p>
-                  <p>Great Is Thy Faithfulness</p>
-                  <p>A Wonderful Change</p>
-                  <p>My Help</p>
-                  <p>Thank You Lord For All You Done For Me</p>
-                  <p>We Are Not Ashamed</p>
-                  <p>Jesus, Jesus, Jesus</p>
-                  <p>Jesus Your the Center of My Joy</p>
-                  <p>For Your Glory</p>
-                  <p>How Great Is Our God</p>
-                  <p>Lord I Lift Your Name On High</p>
-                  <p>Thou Carest Lord</p>
-                  <p>Fill My Cup Lord</p>
-                  <p>More Than Enough</p>
-                  <p>Take Me To The King</p>
-                  <p>Praise Is What I Do</>
-                  <p>Lord Prepare Me To Be A Sanctuary</p>
-                  <p>Many More.....</p>
+                  <p>-You Can Make It</p>
+                  <p>-There Is None like You</p>
+                  <p>-Be Cause He lives</p>
+                  <p>-Because of Who You Are</p>
+                  <p>-We Exalt Thee</p>
+                  <p>-Trust Me</p>
+                  <p>-I Won't Complain</p>
+                  <p>-I Command My Soul Bless the Lord</p>
+                  <p>-Sometimes You Have to Encourage Yourself</p>
+                  <p>-Our God Is An Awesome God</p>
+                  <p>-Let Jesus Fill This Place</p>
+                  <p>-Overflows</p>
+                  <p>-Pure Love</p>
+                  <p>-Glory To Your Name</p>
+                  <p>-Great Is Thy Faithfulness</p>
+                  <p>-A Wonderful Change</p>
+                  <p>-My Help</p>
+                  <p>-Thank You Lord For All You Done For Me</p>
+                  <p>-We Are Not Ashamed</p>
+                  <p>-Jesus, Jesus, Jesus</p>
+                  <p>-Jesus Your the Center of My Joy</p>
+                  <p>-For Your Glory</p>
+                  <p>-How Great Is Our God</p>
+                  <p>-Lord I Lift Your Name On High</p>
+                  <p>-Thou Carest Lord</p>
+                  <p>-Fill My Cup Lord</p>
+                  <p>-More Than Enough</p>
+                  <p>-Take Me To The King</p>
+                  <p>-Praise Is What I Do</>
+                  <p>-Lord Prepare Me To Be A Sanctuary</p>
+                  <p>-Many More.....</p>
               </div>
             </div>
           </div>
